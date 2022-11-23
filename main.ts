1f16f3c701f2a719e72fe3722219322e2403ed15
mpwhite2 <110784371+mpwhite2@users.noreply.github.com>
2022-11-23 14:28:17 -0800

    Update main.blocks, main.ts, images.g.jres, images.g.ts, main.py

--- main.ts
+++ main.ts
@@ -1,14 +1,19 @@
 namespace SpriteKind {
     export const Moon = SpriteKind.create()
     export const Site = SpriteKind.create()
+    export const effect = SpriteKind.create()
 }
 function ChangeImage () {
-      transformSprites.rotateSprite(Lander,transformSprites.getRotation(Lander))
+    transformSprites.rotateSprite(Lander, transformSprites.getRotation(Lander))
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Moon, function (sprite, otherSprite) {
-    if (sprite.vy < 40 && sprite.overlapsWith(Sites2)) {
-        game.over(true)
-    } else {
+    if(!(Lander.overlapsWith(Sites2))){
+        game.splash("THAT'S NOT A LANDING SITE!")
+    }
+    else if (sprite.vy > 10 ) {
+        game.splash("YOU LANDED TOO HARD!")
+    }
+     else {
         game.over(false)
     }
 })
@@ -23,22 +28,23 @@ function thrustLander () {
 function RotateLander (Amount: number) {
     transformSprites.changeRotation(Lander, Amount)
 }
+let fire: Sprite = null
 let thrustY = 0
 let thrustX = 0
 let thrustDirRads = 0
 let thrustDir = 0
-let oldRotation = 0
 let Sites2: Sprite = null
 let Lander: Sprite = null
 let THRUSTER_VELOCITY = 0
+let oldRotation = 0
 THRUSTER_VELOCITY = 20
 Lander = sprites.create(assets.image`myImage`, SpriteKind.Player)
 Lander.setVelocity(13, 0)
 scene.cameraFollowSprite(Lander)
 tiles.setCurrentTilemap(tilemap`level1`)
-let Moon2 = sprites.create(assets.image`Moon`, SpriteKind.Moon)
+let Moon2 = sprites.create(assets.image`Moon2`, SpriteKind.Moon)
 tiles.placeOnTile(Moon2, tiles.getTileLocation(10, 12))
-Sites2 = sprites.create(assets.image`Sites`, SpriteKind.Site)
+Sites2 = sprites.create(assets.image`Sites2`, SpriteKind.Site)
 tiles.placeOnTile(Sites2, tiles.getTileLocation(10, 12))
 game.onUpdateInterval(1, function () {
     if (controller.right.isPressed()) {
@@ -51,15 +57,19 @@ game.onUpdateInterval(1, function () {
 forever(function () {
     if (controller.up.isPressed()) {
         thrustLander()
-        Lander.setImage(assets.image`myImage1`)
-        ChangeImage()
+        fire = sprites.create(assets.image`myImage1`, SpriteKind.effect)
+        fire.setPosition(Lander.x, Lander.y)
+        transformSprites.rotateSprite(fire, transformSprites.getRotation(Lander))
+        Lander.startEffect(effects.spray, 20)
     } else {
         Lander.ax = 0
         Lander.ay = 0
-        Lander.setImage(assets.image`myImage`)
-        ChangeImage()
+        sprites.destroyAllSpritesOfKind(SpriteKind.effect)
     }
 })
 game.onUpdateInterval(100, function () {
     Lander.vy += 1
+})
+game.onUpdateInterval(100, function () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.effect)
 })
